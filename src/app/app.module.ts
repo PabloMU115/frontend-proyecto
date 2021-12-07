24524968b99cc5faed236570df721a438d336c51
Axel <chavarriamontoyaaxe01@gmail.com>
2021-12-07 02:29:50 -0600

    Agregando interfaz del módulo ahorros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CajasModule } from './cajas/cajas.module';
+import { AhorrosModule } from './ahorros/ahorros.module';
 import { SasModule } from './sas/sas.module';
 import { LoginModule } from './login/login.module';
 import { PrestamosModule } from './prestamos/prestamos.module';
@@ -22,7 +23,8 @@ import { NavBarComponent } from './prestamos/component/nav-bar/nav-bar.component
     CajasModule,
     LoginModule,
     PrestamosModule,
-    SasModule
+    SasModule,
+    AhorrosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
